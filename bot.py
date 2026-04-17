495c0d2d78b87b832ccd8ddaf9a3d9439b364b33
bot1master <alighiath73@gmail.com>
2026-04-18 02:16:02 +0300

    fix update error

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from groq import Groq
 
 # 🔑 مفاتيحك
 TOKEN = os.getenv("BOT_TOKEN")
-GROQ_API_KEY = "YOUR_GROQ_API_KEY"
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
 client = Groq(api_key=GROQ_API_KEY)
 
